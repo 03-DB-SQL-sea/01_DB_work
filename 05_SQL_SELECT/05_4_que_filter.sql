0af2bee1ee396290b0f2220c74f261dac111e55f
SEA786 <shams.eltaf786@gmail.com>
2024-08-19 11:02:53 +0200

    05_4 que

--- 05_SQL_SELECT/05_4_que_filter.sql
+++ 05_SQL_SELECT/05_4_que_filter.sql
@@ -10,7 +10,9 @@
 
 -- Eingrenzen/Filtern WHERE & AND/OR etc.
 -- Scharfe Suche
-/**/
+
+
+/*/
 SELECT
 	ticker AS "SYM",
     c_name AS "Unternehmen",
@@ -19,9 +21,10 @@ SELECT
     dividend AS "Dividende",
 	CONCAT(sector, " | ", industry) AS "Operations"
 FROM stocks.ccc
-
+*/
 -- Einzeldaten / Strings / numerisch
 -- WHERE sector = "Communication Services" -- spez. Sektor
+/*/
 WHERE industry = "Media" -- spez. Branche
 -- WHERE payouts = 12 -- Wer zahlt monatlich?
 
@@ -40,11 +43,13 @@ ORDER BY industry DESC
 -- Begrenzung
 LIMIT 20
 ;
-*/
+/*/
 
 -- Eingrenzen/Filtern WHERE & LIKE + Parameter
 -- Unscharfe Suche 
-/*
+
+
+/*/
 SELECT
 	ticker AS "SYM",
     c_name AS Unternehmen,
@@ -52,35 +57,35 @@ SELECT
 FROM stocks.ccc
 
 -- scharfe Suche nach Strings
-#WHERE industry = "Media"
+-- WHERE industry = "Media"
 
 -- unschärfere Suchen --
 -- Branchenname beginnt mit ... , dahinter beliebige Chars
-#WHERE industry LIKE "Air%"
+-- WHERE industry LIKE "Air%"
 -- Branchenname endet mit ... , davor beliebige Chars
-#WHERE industry LIKE "%ment"
+-- WHERE industry LIKE "%ment"
 -- Branchenname enthaelt ...
-#WHERE industry LIKE "%ood%"
+-- WHERE industry LIKE "%ood%"
 
 -- Branchenname endet/beginnt mit ... , danach/davor/inmitten genau _ Char
-#WHERE industry LIKE "__dia"
-#WHERE industry LIKE "Med__"
-#WHERE industry LIKE "M___a"
-#WHERE industry LIKE "_ir%"
-#WHERE industry LIKE "_ood%"
+-- WHERE industry LIKE "__dia"
+-- #WHERE industry LIKE "Med__"
+-- #WHERE industry LIKE "M___a"
+-- #WHERE industry LIKE "_ir%"
+-- WHERE industry LIKE "_ood%"
 
-#WHERE industry LIKE "%ment"
-#WHERE industry LIKE "%ment" AND industry NOT LIKE "%ipment"
-#WHERE industry LIKE "%ment" AND industry NOT LIKE "%ipment" AND industry NOT LIKE "%tain%"
+-- #WHERE industry LIKE "%ment"
+-- #WHERE industry LIKE "%ment" AND industry NOT LIKE "%ipment"
+-- WHERE industry LIKE "%ment" AND industry NOT LIKE "%ipment" AND industry NOT LIKE "%tain%"
 
 
 ORDER BY industry ASC
 LIMIT 40
 ;
-*/
+/*/
 
 --  Eingrenzen/Filtern WHERE & IN / NOT IN (Suchsets)
-/*
+/*/
 SELECT 
     ticker AS "SYM",
     c_name AS "Unternehmen",
@@ -88,24 +93,24 @@ SELECT
     industry AS "Branche"
 FROM stocks.ccc
 
-#WHERE sector = "Financials" 
-#WHERE sector = "Financials" AND industry NOT IN ("Insurance","Banks") 
-WHERE industry IN ("Banks","Beverages")
+-- WHERE sector = "Financials" 
+-- WHERE sector = "Financials" AND industry NOT IN ("Insurance","Banks") 
+-- WHERE industry IN ("Banks","Beverages")
 
 ORDER BY industry ASC
 LIMIT 400 -- X Zeilen ab 0
 ;
-*/
+/*
 
 -- Eingrenzen/Filtern WHERE & RegEx
 /*
 SELECT
 	c_name "Unternehmen"
 FROM stocks.ccc
-#WHERE c_name RLIKE "^[AZ]"  -- mit A oder Z beginnend
+-- WHERE c_name RLIKE "^[AZ]"  -- mit A oder Z beginnend
 WHERE c_name RLIKE "^[1-9]"  -- mit Ziffer beginnend
 ORDER BY c_name;
-*/
+
 
 -- Eingrenzen/Filtern WHERE & BETWEEN / NOT BETWEEN
 /*
@@ -118,13 +123,13 @@ SELECT
 FROM stocks.ccc
 
 #WHERE sector = "Financials" AND price < 30.0  -- =/>/<
-#WHERE sector = "Financials" AND (price BETWEEN 30.0 AND 50.0)  -- =/>/<
+-- WHERE sector = "Financials" AND (price BETWEEN 30.0 AND 50.0)  -- =/>/<
 WHERE sector = "Financials" AND NOT (price BETWEEN 20.0 AND 250.0) -- Band rausfiltern
 
 ORDER BY price DESC
 LIMIT 200 -- X Zeilen ab 0
 ;
-*/
+/*
 
 
 
